586636c3e2579e14b34caa56b40ffe9431aa2920
Wan11703 <122727345+Wan11703@users.noreply.github.com>
2025-05-04 00:42:59 +0800

    user-search
    Co-authored-by: Bread Loaf <BreadLoaf0509@users.noreply.github.com>

--- express-app/public/js/user/user-search.js
+++ express-app/public/js/user/user-search.js
@@ -4,47 +4,47 @@ const searchWrapper = document.querySelector(".search-bar");
 const inputBox = searchWrapper.querySelector("input");
 const drug_list = searchWrapper.querySelector(".drug-list");
 
-inputBox.onkeyup = (e) => {
-    let userData = e.target.value;
-    let emptyArray = [];
-    if (userData) {
-        emptyArray = auto_complete.filter((data) => {
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data) => {
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active");
-        showDrugList(emptyArray);
+// inputBox.onkeyup = (e) => {
+//     let userData = e.target.value;
+//     let emptyArray = [];
+//     if (userData) {
+//         emptyArray = auto_complete.filter((data) => {
+//             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+//         });
+//         emptyArray = emptyArray.map((data) => {
+//             return data = `<li>${data}</li>`;
+//         });
+//         searchWrapper.classList.add("active");
+//         showDrugList(emptyArray);
 
-        let allList = drug_list.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }
-    else {
-        searchWrapper.classList.remove("active");
-    }
-}
+//         let allList = drug_list.querySelectorAll("li");
+//         for (let i = 0; i < allList.length; i++) {
+//             allList[i].setAttribute("onclick", "select(this)");
+//         }
+//     }
+//     else {
+//         searchWrapper.classList.remove("active");
+//     }
+// }
 
-function select(element) {
-    let selectedUserData = element.textContent;
-    inputBox.value = selectedUserData;
-    searchWrapper.classList.remove("active");
-}
+// function select(element) {
+//     let selectedUserData = element.textContent;
+//     inputBox.value = selectedUserData;
+//     searchWrapper.classList.remove("active");
+// }
 
-function showDrugList(list) {
-    let listData;
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    }
-    else {
-        listData = list.join('');
-    }
+// function showDrugList(list) {
+//     let listData;
+//     if (!list.length) {
+//         userValue = inputBox.value;
+//         listData = `<li>${userValue}</li>`;
+//     }
+//     else {
+//         listData = list.join('');
+//     }
 
-    drug_list.innerHTML = listData;
-}
+//     drug_list.innerHTML = listData;
+// }
 
 // modal
 
@@ -80,7 +80,7 @@ open.addEventListener('click', () => {
     // Reset progress bar and start animation
     count = 0;
     per = 0;
-    progress.style.width = "0px"; // Reset progress bar width
+    // progress.style.width = "0px"; // Reset progress bar width
     loading = setInterval(animate, 150); // Start the animation
 
     // Send the drug name to the backend
@@ -121,7 +121,7 @@ close.addEventListener('click', () => {
     clearInterval(loading); // Stop the interval
     count = 0;
     per = 0;
-    progress.style.width = "0px"; // Reset progress bar width
+    // progress.style.width = "0px"; // Reset progress bar width
 
     loaderActive.classList.add("active");
     tabActive.classList.remove("active");
@@ -129,7 +129,7 @@ close.addEventListener('click', () => {
 
 // progress bar
 
-var progress = document.querySelector(".progress");
+// var progress = document.querySelector(".progress");
 var text = document.querySelector(".text");
 
 var count = 0; // Start at 0%
@@ -146,6 +146,6 @@ function animate() {
     text.classList.add("text-blink");
     per = per + 6;
     count = count + 1;
-    progress.style.width = per + "px";
+    // progress.style.width = per + "px";
   }
 }
